2f20f7c9435fd91e9605aeb865d92e5c9499928a
koekigamer <171357@student.aventus.nl>
2023-06-01 14:04:57 +0200

    css werkt nier

--- Checklist.js
+++ Checklist.js
@@ -1,33 +1,8 @@
-const checkboxForm = document.getElementById('checkboxForm');
-const checkboxList = document.getElementById('checkboxList');
-const addItemBtn = document.getElementById('addItemBtn');
-const removeItemBtn = document.getElementById('removeItemBtn');
-const listItems = document.getElementById('listItems');
-
-addItemBtn.addEventListener('click', addItem);
-removeItemBtn.addEventListener('click', removeItem);
-
-function addItem() {
-  const checkedItems = checkboxForm.querySelectorAll('input[type="checkbox"]:checked');
-
-  checkedItems.forEach((item) => {
-    const newItem = document.createElement('li');
-    newItem.textContent = item.value;
-    listItems.appendChild(newItem);
+$('.cb').click(function() {
+  $('ul').html("");
+  $(".cb").each(function(){
+    if($(this).is(":checked")){
+      $('ul').append('<li><input type="checkbox">'+$(this).val()+'</li>')
+    }
   });
-}
-
-function removeItem() {
-  const checkedItems = checkboxForm.querySelectorAll('input[type="checkbox"]:checked');
-
-  checkedItems.forEach((item) => {
-    const value = item.value;
-    const itemsToRemove = listItems.querySelectorAll('li');
-
-    itemsToRemove.forEach((li) => {
-      if (li.textContent === value) {
-        li.remove();
-      }
-    });
   });
-}
