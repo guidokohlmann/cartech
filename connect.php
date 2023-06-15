c97537f90e18a96b6d4d6f062525ce4fd775a5c5
koekigamer <171357@student.aventus.nl>
2023-06-15 10:07:31 +0200

    connect

--- connect.php
+++ connect.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$rawFile = file_get_contents("./config.json");
+$configArray = json_decode($rawFile, true);
+// var_dump($configArray);
+// echo $configArray["servername"];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($configArray["servername"], $configArray["username"], $configArray["password"]);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
 ?>
